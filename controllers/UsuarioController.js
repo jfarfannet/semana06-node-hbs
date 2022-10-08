de5d60d2c9b1fa90b4e43d2abc0636d91947eb4d
Jaime Farfan <jfarfannet@gmail.com>
2022-10-07 19:10:17 -0500

    ADD delete , update

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -30,5 +30,53 @@ usuarioController.save = function(req, res){
     });
 };
 
+usuarioController.edit = function(req, res) {
+    Usuario.findOne({_id: req.params.id}).exec(function (err, usuario) {
+      if (err) { console.log("Error:", err); return; }
+      
+      res.render("../views/usuario/edit", {usuario: usuario});
+      
+    });
+};
+  
+usuarioController.show = function(req, res){
+    Usuario.findOne({_id: req.params.id}).exec(function(err, usuario){
+        if( err ){ console.log('Error: ', err); return; }
+        
+        res.render('../views/usuario/show', {usuario: usuario} );
+    });
+    
+};
+
+usuarioController.update = function(req, res){
+    Usuario.findByIdAndUpdate( req.params.id, {$set: {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email,
+        estado: req.body.estado
+    }}, { new: true },
+    function( err, usuario){
+        if( err ){ 
+            console.log('Error: ', err); 
+            res.render('../views/usuario/edit', {usuario: req.body} );
+        }
+        
+        console.log( usuario );
+        
+        res.redirect('/usuarios/show/' + usuario._id);
+        
+    });
+};
+
+usuarioController.delete = function(req, res){
+    
+    Usuario.remove({_id: req.params.id}, function(err){
+        if( err ){ console.log('Error: ', err); return; }
+        
+        console.log("Usuario deleted!");
+        res.redirect("/usuarios");
+    });
+    
+};
 
 module.exports = usuarioController;
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,5 +6,9 @@ var usuario = require('../controllers/UsuarioController.js');
 router.get('/', usuario.list);
 router.get('/create', usuario.create);
 router.post('/save', usuario.save);
+router.get('/edit/:id', usuario.edit);
+router.get('/show/:id', usuario.show);
+router.post('/update/:id', usuario.update);
+router.post('/delete/:id', usuario.delete);
 
 module.exports = router;
